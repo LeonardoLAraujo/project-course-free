c274ea5034e3f8696fade598232753629576bbc6
Ronaldo Aparecido Augusto Moreira <ronaldoaam@uninove.br>
2025-10-23 10:25:52 -0300

    iniciando a estrutura

---
+++ src/free-course.ts
@@ -0,0 +1,40 @@
+import {LitElement, html, css, TemplateResult, CSSResult} from 'lit';
+import { customElement } from 'lit/decorators.js';
+import './step-screen/step-screen';
+
+@customElement('free-course')
+export default class FreeCourse extends LitElement{
+
+    static override get styles(): CSSResult{
+        return css`
+        
+            :host{
+                display: block;
+                width: 895px;
+                height: 700px;
+            }
+
+            .free-course{
+                width: 100%;
+                height: 100%;
+                border: 1px solid black;
+            }
+        `;
+    }
+
+    protected override render(): TemplateResult{
+        return html`
+            <div class="free-course">
+                <step-screen color="red"></step-screen>
+                <step-screen color="blue"></step-screen>
+            </div>
+        `;
+    }
+
+}
+
+declare global{
+    interface HTMLElementTagNameMap{
+        'course-free': FreeCourse
+    }
+}
